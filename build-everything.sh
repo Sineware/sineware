450ba614d270b29ff7aecc5d0841512ef48f378f
Seshan Ravikumar <seshan10@me.com>
2020-10-29 14:35:15 -0400

    more kernel testing

--- build-everything.sh
+++ build-everything.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir -p build-scripts/output
 rm -rf build-scripts/output/*
 docker build . -t sineware-build
 exec docker run -t -i -v "$(pwd)"/build-scripts:/build-scripts --rm sineware-build
--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -72,7 +72,7 @@ then
   cd $KERNEL_NAME
   echo "Build for ${SINEWARE_ARCH}_defconfig"
   #make ${SINEWARE_ARCH}_defconfig # todo make a kernel .config
-  make allyesconfig # full steam ahead yo
+  make allmodconfig # full steam ahead yo
   make -j$(nproc)
   cp arch/${SINEWARE_ARCH}/boot/bzImage $ROOTFS/boot/bzImage
   make modules_install INSTALL_MOD_PATH=$ROOTFS
