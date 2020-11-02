85504495df1f9b649a8b2719fb0695c02f80f1d1
Seshan Ravikumar <seshan10@me.com>
2020-11-02 16:34:50 -0500

    changes

--- build-iso.sh
+++ build-iso.sh
@@ -2,4 +2,4 @@
 rm -rf iso-build-scripts/output/*
 cp build-scripts/output/sineware.tar.gz iso-build-scripts/files/sineware.tar.gz
 docker build . -t sineware-build
-exec docker run -t -i -v "$(pwd)"/iso-build-scripts:/build-scripts --rm sineware-build
+exec docker run -i -v "$(pwd)"/iso-build-scripts:/build-scripts --rm sineware-build
