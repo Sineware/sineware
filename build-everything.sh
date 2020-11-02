603745ab0da67775a5186758be8af8692f1438a0
Seshan Ravikumar <seshan10@me.com>
2020-11-02 16:28:01 -0500

    Figure out -it later

--- build-everything.sh
+++ build-everything.sh
@@ -3,4 +3,4 @@ set -e
 mkdir -p build-scripts/output
 rm -rf build-scripts/output/*
 docker build . -t sineware-build
-exec docker run -t -i -v "$(pwd)"/build-scripts:/build-scripts --rm sineware-build
+exec docker run -i -v "$(pwd)"/build-scripts:/build-scripts --rm sineware-build
